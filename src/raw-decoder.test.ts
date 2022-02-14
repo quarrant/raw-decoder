e45ab678a7c37dfe27a94c85496be45661d74b2d
Dmitry Alekseev <quarrant@gmail.com>
2022-02-14 16:10:56 +0300

    impl raw decoder

---
+++ src/raw-decoder.test.ts
@@ -0,0 +1,86 @@
+import { RawDecoder } from './raw-decoder';
+
+describe('RawDecoder', () => {
+  it.each([
+    ['', ''],
+    [undefined, undefined],
+  ])('valueOf() is %p', (input, result) => {
+    // @ts-ignore
+    const decoder = new RawDecoder(input);
+    expect(decoder.valueOf()).toEqual(result);
+  });
+
+  it.each([
+    [{ id: '' }, ''],
+    [{} as { id: string }, undefined],
+  ])('valueOf() is %p', (input, result) => {
+    // @ts-ignore
+    const decoder = new RawDecoder(input);
+    expect(decoder.getProperty('id').valueOf()).toEqual(result);
+  });
+
+  it.each([
+    [undefined, ''],
+    [null, ''],
+    [[], ''],
+    [{}, ''],
+    ['abc', 'abc'],
+    [123, '123'],
+  ])('asString(%p) is %p', (input, result) => {
+    // @ts-ignore
+    const decoder = new RawDecoder(input);
+    expect(decoder.asString()).toEqual(result);
+  });
+
+  it.each([
+    [undefined, 0],
+    [null, 0],
+    ['1', 1],
+    [NaN, 0],
+    [123, 123],
+  ])('asNumber(%p) is %p', (input, result) => {
+    // @ts-ignore
+    const decoder = new RawDecoder(input);
+    expect(decoder.asNumber()).toEqual(result);
+  });
+
+  it.each([
+    [undefined, false],
+    [null, false],
+    [0, false],
+    [1, true],
+    ['', false],
+    [NaN, false],
+    ['true', true],
+    ['false', true],
+  ])('asBoolean(%p,%p) is %p', (input, result) => {
+    // @ts-ignore
+    const decoder = new RawDecoder(input);
+    expect(decoder.asBoolean()).toEqual(result);
+  });
+
+  it.each([
+    [{}, (r: RawDecoder<{}>) => r, []],
+    [null, (r: RawDecoder<{}>) => r, []],
+    [undefined, (r: RawDecoder<{}>) => r, []],
+    [[1], (r: RawDecoder<number>) => r.asNumber(), [1]],
+    [['1'], (r: RawDecoder<number>) => r.asString(), ['1']],
+    [[{ a: 'a' }], (r: RawDecoder<{ a: 'a' }>) => r.getProperty('a').asString(), ['a']],
+  ])('asArray(%p, %s) is %p', (input, iteratte, result) => {
+    // @ts-ignore
+    const decoder = new RawDecoder(input);
+    // @ts-ignore
+    expect(decoder.asArray(iteratte)).toEqual(result);
+  });
+
+  it.each([
+    ['{}', {}],
+    ['', undefined],
+    [null, undefined],
+    [undefined, undefined],
+  ])('asJSON(%p) is %p', (input, result) => {
+    // @ts-ignore
+    const decoder = new RawDecoder(input);
+    expect(decoder.asJSON()).toEqual(result);
+  });
+});
---
+++ src/raw-decoder.ts
@@ -0,0 +1,69 @@
+type ArrayItemType<ArrayType> = ArrayType extends readonly (infer ElementType)[] ? ElementType : never;
+
+export class RawDecoder<Raw> {
+  constructor(private readonly raw: Raw | undefined) {}
+
+  public getProperty<Property extends keyof NonNullable<Raw>>(
+    property: Property,
+  ): RawDecoder<NonNullable<Raw>[Property]> {
+    if (!(this.raw as any)?.hasOwnProperty(property)) {
+      console.warn(`[RawDecoder/getProperty] property '${property}' not found in raw '${JSON.stringify(this.raw)}'`);
+    }
+
+    return new RawDecoder(this.raw?.[property]);
+  }
+
+  public valueOf() {
+    return this.raw;
+  }
+
+  public asString() {
+    if (typeof this.raw === 'string' || typeof this.raw === 'number') {
+      return String(this.raw);
+    }
+
+    return '';
+  }
+
+  public asNumber() {
+    if (typeof this.raw === 'string' || typeof this.raw === 'number' || typeof this.raw === 'bigint') {
+      const value = Number(this.raw);
+      if (!Number.isNaN(value)) {
+        return value;
+      }
+    }
+
+    return 0;
+  }
+
+  public asBoolean() {
+    if (typeof this.raw === 'undefined' || this.raw === null) {
+      return false;
+    }
+
+    return Boolean(this.raw);
+  }
+
+  public asArray<ArrayItem>(iteratte: (r: RawDecoder<ArrayItemType<Raw>>) => ArrayItem): ArrayItem[] {
+    if (Array.isArray(this.raw)) {
+      return this.raw.map((r: ArrayItemType<Raw>) => {
+        const decoder = new RawDecoder(r);
+        return iteratte(decoder);
+      });
+    }
+
+    return [];
+  }
+
+  public asJSON(): Record<string, any> | undefined {
+    try {
+      if (typeof this.raw === 'string') {
+        return JSON.parse(this.raw);
+      }
+    } catch (error) {
+      console.warn(`[decodeJSON] raw=${this.raw}`, error);
+    }
+
+    return undefined;
+  }
+}
